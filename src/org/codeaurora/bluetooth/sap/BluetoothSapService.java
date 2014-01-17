85f24ee5aae6dae60aaad2e1ea60543d7a7f1524
Smriti Gupta <smritig@codeaurora.org>
2014-01-18 00:12:52 +0530

    Revert "SAP: Handle ACL_DISCONNECT from the client"
    This reverts commit dc7d732a106affb1cd98e4de6cc5a20e105015e6.
    
    Change-Id: Ib0e91ee5ebffe74a945609ad5b7cfd5991f378fe
    CRs-Fixed: 596152

--- src/org/codeaurora/bluetooth/sap/BluetoothSapService.java
+++ src/org/codeaurora/bluetooth/sap/BluetoothSapService.java
@@ -485,22 +485,18 @@ public class BluetoothSapService extends Service {
             } else {
                 removeTimeoutMsg = false;
             }
-        } else if (action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED)) {
+        } else if (action.equals(BluetoothDevice.ACTION_ACL_DISCONNECTED) &&
+                                 mIsWaitingAuthorization) {
             if (mRemoteDevice == null) {
-                Log.e(TAG, "Unexpected error!");
-                return;
+               Log.e(TAG, "Unexpected error!");
+               return;
             }
             if (mSapHandler != null) {
-                /* Let the user timeout handle this case as well */
-                if (mIsWaitingAuthorization) {
-                    mSapHandler.sendMessage(mSapHandler.obtainMessage(MESSAGE_SAP_USER_TIMEOUT));
-                    removeTimeoutMsg = false;
-                } else {
-                    mSapHandler.sendMessage(mSapHandler.obtainMessage(SAP_CRTL_MSG_DISCONNECT_REQ));
-                    closeRfcommSocket();
-                    startRfcommListenerThread();
-                }
+               /* Let the user timeout handle this case as well */
+               mSapHandler.sendMessage(mSapHandler.obtainMessage(MESSAGE_SAP_USER_TIMEOUT));
+               removeTimeoutMsg = false;
             }
+
         }  else if (action.equals(SAP_ACCESS_ALLOWED_ACTION)) {
             if (mRemoteDevice == null) {
                Log.e(TAG, "Unexpected error!");
