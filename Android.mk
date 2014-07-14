8bc5d918c489fe33e631b7dd073300642c149c02
Hemant Gupta <hemantg@codeaurora.org>
2014-07-14 16:35:54 +0530

    Disable ProximityService, a4wp and btceservice
    Disable ProximityService, a4wp and btceservice
    
    Change-Id: If5ee8bc923d3b779123cce413ba9005c2a43df3f

--- Android.mk
+++ Android.mk
@@ -5,19 +5,19 @@ LOCAL_MODULE_TAGS := optional
 src_dirs:= src/org/codeaurora/bluetooth/btcservice \
            src/org/codeaurora/bluetooth/ftp \
            src/org/codeaurora/bluetooth/dun \
-           src/org/codeaurora/bluetooth/pxpservice \
-           src/org/codeaurora/bluetooth/a4wp
+           #src/org/codeaurora/bluetooth/pxpservice \
+           #src/org/codeaurora/bluetooth/a4wp
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, $(src_dirs)) \
-        src/org/codeaurora/bluetooth/pxpservice/IPxpService.aidl
+        #src/org/codeaurora/bluetooth/pxpservice/IPxpService.aidl
 
 LOCAL_PACKAGE_NAME := BluetoothExt
 LOCAL_CERTIFICATE := platform
 LOCAL_JAVA_LIBRARIES := javax.obex
 LOCAL_JAVA_LIBRARIES += mms-common
 LOCAL_JAVA_LIBRARIES += telephony-common
-LOCAL_JAVA_LIBRARIES += com.quicinc.wbc
+#LOCAL_JAVA_LIBRARIES += com.quicinc.wbc
 
 LOCAL_STATIC_JAVA_LIBRARIES := com.android.vcard
 
