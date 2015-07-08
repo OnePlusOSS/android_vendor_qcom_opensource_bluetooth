d68ec609b6cf26ba55536489d9baf1442a030af8
Hemant Gupta <hemantg@codeaurora.org>
2015-07-08 23:58:51 +0530

    PXP: Disable proximity code to fix compilation issues
    This patch disables proximity code to fix compilation issues
    
    Change-Id: I481aa1b420eb33cde5fd13ec87920a17e62f640f

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE_TAGS := optional
 src_dirs:= src/org/codeaurora/bluetooth/btcservice \
            src/org/codeaurora/bluetooth/ftp \
            src/org/codeaurora/bluetooth/dun \
-           src/org/codeaurora/bluetooth/pxpservice
+           #src/org/codeaurora/bluetooth/pxpservice
 
 ifeq ($(BOARD_USES_WIPOWER),true)
            src_dirs += src/org/codeaurora/bluetooth/a4wp
@@ -15,7 +15,7 @@ endif
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, $(src_dirs)) \
-        src/org/codeaurora/bluetooth/pxpservice/IPxpService.aidl
+        #src/org/codeaurora/bluetooth/pxpservice/IPxpService.aidl
 
 LOCAL_PACKAGE_NAME := BluetoothExt
 LOCAL_CERTIFICATE := platform
